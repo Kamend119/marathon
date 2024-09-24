cfadb4546bd16cc9ee04854df8cf8eb0a104ad9a
kamend119 <a.mikryukova119@mail.ru>
2024-09-24 14:23:55 +0500

    Добавьте файлы проекта.

---
+++ marathon/pagesCoordinator/uploadingVolunteersPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using marathon.pagesGeneral;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace marathon.pagesCoordinator
+{
+    public partial class uploadingVolunteersPage : Page
+    {
+        public uploadingVolunteersPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new mainPage());
+        }
+    }
+}
